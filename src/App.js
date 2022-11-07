ff6dbbce305429aee352d6fa855aa968a736a2ba
Jrdc12 <juliodc289@gmail.com>
2022-11-07 10:08:18 -0500

    Added context features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
 import Layout from "./components/layout/Layout";
+import { FavoritesContextProvider } from "./store/favorites-context";
 
 function App() {
 	return (
 		<div>
-			<BrowserRouter>
-				<Layout>
-					<Routes>
-						<Route path="/" element={<AllMeetupsPage />} />
-						<Route path="/new-meetup" element={<NewMeetupPage />} />
-						<Route path="/favorites" element={<FavoritesPage />} />
-					</Routes>
-				</Layout>
-			</BrowserRouter>
+			<FavoritesContextProvider>
+				<BrowserRouter>
+					<Layout>
+						<Routes>
+							<Route path="/" element={<AllMeetupsPage />} />
+							<Route path="/new-meetup" element={<NewMeetupPage />} />
+							<Route path="/favorites" element={<FavoritesPage />} />
+						</Routes>
+					</Layout>
+				</BrowserRouter>
+			</FavoritesContextProvider>
 		</div>
 	);
 }
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,20 @@
+import { useContext } from "react";
+
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/meetups/MeetupList";
+
 function FavoritesPage() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
-    <h1>Favorites</h1>
+    <section>
+      <h1>My Favorites</h1>
+      {favoritesCtx.totalFavorites === 0 ? (
+        <p>You got no favorites yet. Start adding some?</p>
+      ) : (
+        <MeetupList meetups={favoritesCtx.favorites} />
+      )}
+    </section>
   )
 }
 
